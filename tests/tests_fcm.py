b23836e2f52eca46d24d2a93b00be51193d49c3a
Thales Aguiar <thalesaguiar21@gmail.com>
2019-10-01 09:02:30 -0300

    Added blob dataset for tests

--- tests/tests_fcm.py
+++ tests/tests_fcm.py
@@ -1,18 +1,18 @@
 from .context import fuzzy
 import fuzzy.models.fcm as fcm
 import unittest
-import numpy
-import sklearn.datasets as skdata
+import numpy as np
 
 
 class TestsFCM(unittest.TestCase):
 
     def setUp(self):
         self.mfcm = fcm.FCM(3, 2)
-        points, labels = skdata.make_blobs(500, 2, 3)
-        self.data = points
-        self.labels = labels
+        dataset = np.loadtxt('/home/thales/DEV/Fuzzy/tests/dataset_blob.txt')
+        self.data = dataset[:, :-1]
+        self.labels = dataset[:, :-1]
 
     def test_partitions_dim(self):
         parts, _ = self.mfcm.fit(self.data, 0.2)
         self.assertEqual(parts.shape, (500, 3))
+
