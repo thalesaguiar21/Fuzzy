aac5a4d9fd74b85312bbc353d3eb928f84be57da
Thales Aguiar <thalesaguiar21@gmail.com>
2019-10-04 10:25:02 -0300

    Improved mixture weights computation and using list slices instead of tranposed partitions

--- fuzzy/clustering.py
+++ fuzzy/clustering.py
@@ -86,15 +86,14 @@ class FGMM:
     def fit(self, data, fuzzyness=2, tolerance=0.2):
         fcm = FCM(self.ncomponents, fuzzyness)
         partitions, _ = fcm.fit(data, tolerance)
-        partitions_T = partitions.T
         loglike = 3
         threshold = 2
         while loglike > threshold:
-            mixture_weights = _compute_mixture_weights(partitions_T)
+            mixture_weights = _compute_mixture_weights(partitions)
             pca_components = data.shape[1]
             pca = skdecomp.PCA(n_components=pca_components)
             for i in range(self.ncomponents):
-                weighted_data = (partitions_T[i] * data.T).T
+                weighted_data = (partitions[:, i] * data.T).T
                 transformed_points = pca.fit_transform(weighted_data)
                 # Take first and second dimension of transformed X, that is Y
                 v1s = transformed_points[:, 0]
@@ -124,7 +123,6 @@ class FGMM:
 
 def _compute_mixture_weights(partitions):
     cluster_relevance = np.sum(partitions, axis=0)
-    # Update to sum cluster_relvance instead of partitions
-    total_weight = np.sum(partitions)
+    total_weight = np.sum(cluster_relevance)
     return cluster_relevance / total_weight
 
