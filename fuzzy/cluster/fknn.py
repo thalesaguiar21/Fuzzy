74af1371f2e851b6d5e68a108e03461e7c56a84d
Thales Aguiar de Lima <thalesaguiar21@gmail.com>
2020-03-09 16:09:28 -0300

    Implemented the crisp version of the algorithm

--- fuzzy/cluster/fknn.py
+++ fuzzy/cluster/fknn.py
@@ -1,6 +1,41 @@
 import numpy as np
 
 
+def fknn(ukn, X, Y, k, p):
+    n_points, dim = X.shape
+    k = clip(k, 1, n_points)
+    if Y.size != n_points:
+        raise ValueError('Different number of labels and samples')
+
+    neighbours = [Y[0]]
+    dists = [minkowski(ukn, X[0], p)]
+    for i in range(1, k):
+        dist = minkowski(ukn, X[i], p)
+        j = 0
+        while j < i and dist > dists[j]:
+            j += 1
+        dists.insert(j, dist)
+        neighbours.insert(j, Y[i])
+
+    i = k + 1
+    while i < n_points:
+        dist = minkowski(ukn, X[i])
+        if i < k:
+            dists.append(dist)
+        else:
+            for j in range(len(dists)):
+                if dists[j] > dist:
+                    dists.append(dist)
+                    dists.pop(j)
+                    neighbours.pop(j)
+                    neighbours.insert(j, Y[i])
+        i += 1
+
+
+def clip(x, lower, higher):
+    return min(max(x, lower), higher)
+
+
 def minkowski(a, b, p):
     ''' Computes the Minkowki distance between two points
     '''
@@ -9,4 +44,9 @@ def minkowski(a, b, p):
         raise ValueError('Cannot compute with different array dimensions')
     pow_dists = np.abs(a - b)**p
     return np.abs(pow_dists).sum() ** (1.0/p)
+
+
+X, Y = np.arange(10).reshape(5, 2), np.arange(5)
+ukn = np.array([1, 2])
+fknn(ukn, X, Y, 10, 2)
 
