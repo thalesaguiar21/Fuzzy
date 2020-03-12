e0eda7412629d89c90531903c6e9f6680d3c6f80
Thales Aguiar de Lima <thalesaguiar21@gmail.com>
2020-03-12 14:01:09 -0300

    Added membership degrees initialisation

--- fuzzy/cluster/fknn.py
+++ fuzzy/cluster/fknn.py
@@ -15,17 +15,39 @@ class FKNN:
     def __init__(self, k, p, tree=None):
         self.k = k
         self.p = p
-        self._memberships = []
+        self._mdegrees = []
         self._tree = tree
 
     def fit(self, X, Y):
-        labeled_data = np.hstack((X, Y)).tolist()
-        self._tree = kdtree.build(labeled_data)
+        self._tree = _organise_data(X, Y)
+        self._compute_mdegrees(X, Y)
+
+    def _compute_mdegrees(self, X, Y):
+        n_classes, n_samples = len(np.unique(Y)), Y.shape[0]
+        self._mdegrees = np.zeros((n_samples, n_classes))
+        for i in range(n_samples):
+            neighs = kdtree.find_neighbours(self._tree, X[i], self.k, self.p)
+            cls_count = np.zeros(n_classes)
+            for lbl in _labels(neighs):
+                cls_count[lbl] += 1
+            self._mdegrees[i] = self._mdegree(cls_count, Y[i])
+
+    def _mdegree(self, cls_count, cls):
+        mdegree = (0.49/self.k) * cls_count
+        mdegree[cls] += 0.51
+        return mdegree
 
     def predict(self, x):
-        neighbours = kdtree.find_neighbours(self._tree, x, self.k, self.p)
-        pred = getmostfrequent(neighbours[:][-1])
-        return pred
+        return -1
+
+
+def _organise_data(X, Y):
+    labeled_data = np.hstack((X, Y))
+    return kdtree.build(labeled_data.tolist())
+
+
+def _labels(x):
+    return x[:, -1]
 
 
 def getmostfrequent(sequence):
