ac6d19f0f536b35adb2431de690e0fc3380c941c
Thales Aguiar de Lima <thalesaguiar21@gmail.com>
2020-03-13 16:11:09 -0300

    Removed Minkowski metric and tests from project

--- fuzzy/cluster/fknn.py
+++ fuzzy/cluster/fknn.py
@@ -101,12 +101,3 @@ def _points(x):
 def clip(x, lower, higher):
     return min(max(x, lower), higher)
 
-
-def minkowski(a, b, p):
-    """ Computes the Minkowki distance between two points """
-    a, b = np.array(a), np.array(b)
-    if a.shape != b.shape:
-        raise ValueError('Cannot compute with different array dimensions')
-    pow_dists = np.abs(a - b)**p
-    return pow_dists.sum() ** (1.0/p)
-
