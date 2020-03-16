0e077804f675a2cb9e10bcb7b380e27d903f3cc5
Thales Aguiar de Lima <thalesaguiar21@gmail.com>
2020-03-16 11:38:35 -0300

    Added test with blob data for fknn

--- tests/tests_fknn.py
+++ tests/tests_fknn.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 import numpy as np
@@ -5,14 +6,24 @@ import numpy as np
 from .context import fuzzy
 from fuzzy.cluster.fknn import FKNN
 
+DIRNAME = os.path.dirname(__file__)
+FILEPATH = os.path.join(DIRNAME, 'dataset_blob.txt')
+
 class TestsFKNN(unittest.TestCase):
 
-    def test_run(self):
-        X = np.arange(10).reshape(5, 2)
-        Y = np.array([0, 0, 1, 1, 1]).reshape(5, 1)
-        ukn = np.array([1, 2])
+    def load_blob(self):
+        self.dataset = np.loadtxt(FILEPATH)
+        self.Xtrain = self.dataset[:350, :-1]
+        self.Xtest = self.dataset[350:, :-1]
+        self.Ytrain = self.dataset[:350, -1]
+        self.Ytest = self.dataset[350:, -1]
+
+        ntrainsamples= self.Ytrain.size
+        self.Ytrain = self.Ytrain.reshape(ntrainsamples, 1)
+
+    def test_fit(self):
+        data = self.load_blob()
         model = FKNN(2, 2, 2)
-        model.fit(X, Y)
-        pred = model.predict(ukn)
+        model.fit(self.Xtrain, self.Ytrain)
 
 
