f5bde1d41bb1455fbeb2c23e924f93c9cd06584f
Thales Aguiar <thalesaguiar21@gmail.com>
2020-03-29 10:38:49 -0300

    Made mfs spam over feature space

--- anfis.py
+++ anfis.py
@@ -5,8 +5,10 @@ from itertools import product
 from sklearn.datasets import make_blobs
 from sklearn.preprocessing import normalize
 import numpy as np
+import matplotlib.pyplot as plt
 
 from fuzzy import lse
+from fuzzy.logic import mfs
 
 
 # Make test data
@@ -16,7 +18,7 @@ N_POINTS, N_INPUTS = X.shape
 
 # Set constants
 MAX_EPOCH = 10
-FSET_SIZE = 10
+FSET_SIZE = 3
 STEP_SIZE = 1e-10
 TOL = 0.2
 N_MFS = N_INPUTS * FSET_SIZE
@@ -32,21 +34,33 @@ print(f"Max epochs: {MAX_EPOCH}\t\t"
 
 
 def build_mfs(inputs, nmfs=2):
-    for mean in _find_means(inputs, nmfs):
-        breakpoint()
+    feat_spams = _find_feat_spam(inputs)
+    mins = np.min(inputs, axis=0)
+    means = _find_means(feat_spams, mins, nmfs)
+    mfspam = np.repeat(feat_spams, nmfs)
+    plato = np.ones(means.shape)
+    params = np.array([mfspam, plato, means])
+    return params.T
 
 
-def _find_means(inputs, nmfs):
+def _find_feat_spam(inputs):
     mins = np.min(inputs, axis=0)
     maxs = np.max(inputs, axis=0)
     inp_spaces = np.abs(mins) + np.abs(maxs)
-    stepsizes = inp_spaces / (nmfs-1)
-    splits = np.arange(nmfs)
+    return inp_spaces
+
+
+def _find_means(featspam, mins, nmfs):
+    stepsizes = featspam / (nmfs-1)
+    stepsizesrow = stepsizes.reshape(1, len(stepsizes))
+    steps = np.arange(nmfs).reshape(nmfs, 1)
+    splits = steps @ stepsizesrow + mins
+    return splits.T.reshape(-1)
 
 
 def half_forward_pass(x):
     inps = np.repeat(x, FSET_SIZE)
-    args = (inps - premise[:, 0])/premise[:, 1]
+    args = (inps - premises[:, 0])/premises[:, 1]
     l1 = np.exp(-args**2)
     mfids = np.arange(N_MFS).reshape((N_INPUTS, FSET_SIZE))
     l2 = []
@@ -61,9 +75,7 @@ def half_forward_pass(x):
     return l1, l2, l3
 
 
-premise_mean = build_mfs(X, FSET_SIZE)
-premise_std = np.ones((N_MFS, 1))
-premise = np.hstack((premise_mean, premise_std))
+premises = build_mfs(X, FSET_SIZE)
 sysmat = []
 
 print('\nFinding consequent parameters...')
@@ -106,9 +118,9 @@ while epoch < MAX_EPOCH and not converged:
         do3_do2 = do2_do1 * fi
 
         xpropagated = np.repeat(x, FSET_SIZE) 
-        expderiv_c = (xpropagated-premise[:, 0]) / premise[:, 1]**2
+        expderiv_c = (xpropagated-premises[:, 0]) / premises[:, 1]**2
         do1_dc = 2*(expderiv_c) * o1
-        expderiv_a = (xpropagated-premise[:, 0])**2 / premise[:, 1]**3
+        expderiv_a = (xpropagated-premises[:, 0])**2 / premises[:, 1]**3
         do1_da = 2*(expderiv_a) * o1
 
         grad_aux = de_do5 * do2_do1
@@ -118,8 +130,8 @@ while epoch < MAX_EPOCH and not converged:
 
     eta = STEP_SIZE / math.sqrt(np.sum(premise_grad**2))
     learnrate = -eta * premise_grad
-    premise += (learnrate * premise_grad).T
-    premise = np.fmax(premise, np.finfo(np.float64).eps)
+    premises += (learnrate * premise_grad).T
+    premises = np.fmax(premises, np.finfo(np.float64).eps)
 
     curr_sqerror = sum((Y - o5)**2)
     loss = abs(error - curr_sqerror)
